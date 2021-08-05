e80de302fdf7f10903b67f392b0d5c3ae01e3450
Cindy <cindysu2@illinois.edu>
2021-08-05 12:33:58 +0800

    third commit

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+import { GLTFLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
+
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xf6f6f6)
 const light = new THREE.AmbientLight( 0x404040, 5 ); // soft white light
 scene.add( light );
 const pointLight = new THREE.PointLight( 0xff0000, 1, 100 );
@@ -32,11 +35,37 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 renderer.shadowMap.enabled = true
 
+
+let fish
+let theLoader
+let Mixer
+const clock = new THREE.Clock()
 function animate(){
     requestAnimationFrame(animate)
     mesh.rotation.x += 0.01
     mesh.rotation.y += 0.01
+    var dt = clock.getDelta()
+    if(Mixer){
+        Mixer.update(dt)
+    }
     renderer.render(scene, camera)
 }
 
+function addFish(){
+  theLoader = new GLTFLoader()
+  theLoader.load(
+    './clown_fish (1)/scene.gltf',
+    gltf => {
+        fish = gltf.scene
+        fish.scale.set(1,1,1)
+        fish.position.set(0, 2, -2)
+        fish.rotation.y += Math.PI/2;
+        Mixer = new THREE.AnimationMixer(fish)
+        Mixer.clipAction(gltf.animations[0]).play();
+      scene.add(fish);
+    }
+  )
+}
+
+addFish()
 animate()
